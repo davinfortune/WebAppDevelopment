96724b4def860a999f3425c9a7858ab5a5ad3fde
davinfortune <davinfortune@gmail.com>
2021-09-27 15:51:51 +0100

    Finished Exercise 4

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -2,9 +2,55 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+let modules = [
+  {
+    name: "DevOps",
+    noLectures: 2,
+    noPracticals: 3
+  },
+  {
+    name: "Enterprise Web Dev",
+    noLectures: 3,
+    noPracticals: 2
+  },
+  {
+    name: "Programming",
+    noLectures: 2,
+    noPracticals: 2
+  },
+  {
+    name: "Computer Systems",
+    noLectures: 2,
+    noPracticals: 3
+  }
+]
+
+const Demo = props => {
+
+  const TableData =  props.modules.map(module => {
+    return (
+      <tr>
+        <td>{module.name}</td>
+        <td>{module.noLectures}</td>
+        <td>{module.noPracticals}</td>
+      </tr>
+    );
+  });
+
   return (
-      <h1>TODO</h1>
+    <div>
+        <h1>HDip Computer Science - Module table</h1>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>No lectures</th>
+              <th>No practicals</th>
+            </tr>
+          </thead>
+          <tbody>{TableData}</tbody>
+      </table>
+    </div>
   );
 };
 
