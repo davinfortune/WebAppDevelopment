0f0f85181de3dc276059b0eb1c2b8c88c3c08b87
davinfortune <davinfortune@gmail.com>
2021-09-27 15:09:33 +0100

    Exercise 3 Finished

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -1,10 +1,48 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+let modules = [
+  {
+    name: "DevOps",
+    noLectures: 2,
+    noPracticals: 3
+  },
+  {
+    name: "Enterprise Web Dev",
+    noLectures: 3,
+    noPracticals: 2
+  }
+]
 
 const Demo = () => {
+
+  const Row = (props) => {
+    return (
+      <tr>
+        <td>{props.name}</td>
+        <td>{props.noLectures}</td>
+        <td>{props.noPracticals}</td>
+      </tr>
+    );
+  }
+
   return (
-      <h1>TODO</h1>
+    <div>
+    <h1>HDip Computer Science - Module table</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Row name = {modules[0].name} noLectures = {modules[0].noLectures} noPracticals = {modules[0].noPracticals}/>
+        <Row name = {modules[1].name} noLectures = {modules[1].noLectures} noPracticals = {modules[1].noPracticals}/>
+      </tbody>
+  </table>
+</div>
   );
 };
 
